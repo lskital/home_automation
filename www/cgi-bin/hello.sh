886aae563d0280263e1c23de848bfff1c0400be5
lsktial <lsktial@gmail.com>
2017-05-14 11:15:10 +0000

    www files sync.

---
+++ www/cgi-bin/hello.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Content-type: text/html"
+echo ""
+
+echo '<html>'
+echo '<head>'
+echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
+echo '<title>Hello World</title>'
+echo '</head>'
+echo '<body>'
+echo 'Hello World ????'
+echo $(whoami)
+echo '<br>'
+echo $*
+echo '<br>'
+echo '<pre>'
+/usr/bin/env
+echo '</pre>'
+echo '</body>'
+echo '</html>'
+
+exit 0
